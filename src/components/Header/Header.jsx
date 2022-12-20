0eaceb5d89ae840764c7e77d6ffe822c815be6ec
farishy <muhammadfaris9d@gmail.com>
2022-12-20 20:35:41 +0700

    auth, breakpoint validation

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,71 @@
+import {
+  AppBar,
+  Box,
+  CssBaseline,
+  Divider,
+  Drawer,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
+import Icon from "@mui/material/Icon";
+
+const navItems = ["Home", "About", "Contact"];
+const drawerWidth = 240;
+const Header = (props) => {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+  // const container =
+  //   window !== undefined ? () => window().document.body : undefined;
+  return (
+    <div>
+      <CssBaseline />
+      <AppBar component="nav" style={{ background: "rgba(0,0,0,0.5)" }}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{
+              flexGrow: 1,
+              display: { xs: "none", sm: "block" },
+              fontFamily: "Rubik Gemstones",
+            }}
+          >
+            Magic Memory
+          </Typography>
+          {/* <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            {navItems.map((item) => (
+              <Button key={item} sx={{ color: "#fff" }}>
+                {item}
+              </Button>
+            ))}
+          </Box> */}
+          <div className="d-flex gap-2">
+            <IconButton sx={{ color: "white" }}>
+              <Icon>music_note</Icon>
+            </IconButton>
+            <IconButton sx={{ color: "white" }}>
+              <Icon>leaderboard</Icon>
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+export default Header;
